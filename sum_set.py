cdf5e4005af40fe26eb5085fbe377bac89804711
Milton Bravo <miltonbravo96@gmail.com>
2019-08-21 21:54:38 -0500

    parcialmente aceptado

---
+++ sum_set.py
@@ -0,0 +1,15 @@
+n = int(input())
+a = list(map(int, input().split()))
+m = int(input())
+c = list(map(int, input().split()))
+b = []
+for i in c:
+    for k in a:
+        if i-k > 0 and i-k not in b:
+            b.append(i-k)
+b.sort()
+for i in a:
+    for j in b:
+        if i+j not in c:
+            b.remove(j)
+print(*b)
