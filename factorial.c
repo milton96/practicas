640e94076a218b3feffa770e82b918fe063736e1
Milton Bravo <miltonbravo96@gmail.com>
2019-08-21 20:18:54 -0500

    factorial con recursividad en c

---
+++ factorial.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int N;
+    scanf("%d", &N);
+    printf("%d", f(N));
+    system("pause");
+    return 0;
+}
+
+int f(x){
+    if(x==0){
+        return 1;
+    }
+    return x*f(x-1);
+}
