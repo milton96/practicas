2e0161ee008ae31ad27ac6398e3aff7cf8e8ec5d
Milton Bravo Sanchez <mbravo@isavanzados.com.mx>
2019-08-22 10:06:53 -0500

    ejercicio aceptado

--- sum_set.py
+++ sum_set.py
@@ -2,14 +2,12 @@ n = int(input())
 a = list(map(int, input().split()))
 m = int(input())
 c = list(map(int, input().split()))
-b = []
-for i in c:
-    for k in a:
-        if i-k > 0 and i-k not in b:
-            b.append(i-k)
-b.sort()
-for i in a:
-    for j in b:
-        if i+j not in c or j+2 not in c:
-            b.remove(j)
-print(*b)
+for i in range(max(max(a), max(c))):
+    d = 0
+    for j in range(n):
+        for k in range(m):
+            if i+a[j] is c[k]:
+                d += 1
+                break
+    if d is n:
+        print(i, '', end='')
