ec3e054601c255624fa8b9b5dd509aad07c2df80
Milton Bravo <miltonbravo96@gmail.com>
2019-08-21 20:20:06 -0500

    ejercicio completo

---
+++ profile_picture.py
@@ -0,0 +1,13 @@
+l = int(input())
+n = int(input())
+while n > 0:
+    w, h = map(int, input().split())
+    if w < l or h < l:
+        print("UPLOAD ANOTHER")
+    elif w == l and h == l:
+        print("ACCEPTED")
+    elif w == h and w >= l:
+        print("ACCEPTED")
+    else:
+        print("CROP IT")
+    n -= 1
