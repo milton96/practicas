cb18712dc4b6fdea9cb23e2115a5a9d4317ce5d1
Milton Bravo Sanchez <mbravo@isavanzados.com.mx>
2019-08-23 13:20:18 -0500

    ejercicio aceptado

--- bishu_and_soldiers.py
+++ bishu_and_soldiers.py
@@ -1,13 +1,11 @@
 from bisect import bisect
 from itertools import accumulate
 n = int(input())
-arr = []
+a = []
 for _ in range(n):
-    line = int(input())
-    arr.append(line)
-arr.sort()
-arr1 = list(accumulate(arr))
+    a.append(int(input()))
+a.sort()
+b = list(accumulate(a))
 for i in range(int(input())):
-    x = int(input())
-    ans = bisect(arr,x)
-    print(ans,arr1[ans-1])
+    r = bisect(a,int(input()))
+    print(r,b[r-1])
