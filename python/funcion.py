d17488131e7a09fc0e59cf4a537450d23c939dc6
Milton Bravo Sanchez <mbravo@isavanzados.com.mx>
2019-09-04 15:37:25 -0500

    ejercicios python

---
+++ python/funcion.py
@@ -0,0 +1,6 @@
+def suma(num1, num2):
+    '''Funcion que suma dos numeros'''
+    return num1 + num2
+
+
+print(suma(2, 4))
---
+++ python/string_format.py
@@ -0,0 +1,3 @@
+frutas = ["manzanas", "naranjas", "mangos", "uvas"]
+for fruta in frutas:
+    print("Fruta: {fruta}, peso: {peso}".format(fruta = fruta, peso = len(fruta)))
