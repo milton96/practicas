1e8ce1310df259ba9425fe10645a0512db86fe38
Milton Bravo Sanchez <mbravo@isavanzados.com.mx>
2019-09-12 11:59:18 -0500

    ejercicio aceptado, mejor tiempo de respuesta

--- Greedy/exploring_ruins.py
+++ Greedy/exploring_ruins.py
@@ -1,24 +1,22 @@
 s = list(input())
 n = len(s)
 if n > 1:
-    for i in range(n):
+    if s[0] == '?':
+        if s[1] == 'a':
+            s[0] = 'b'
+        else:
+            s[0] = 'a'
+    for i in range(1, n-1):
         if s[i] == '?':
-            if i == 0:
-                if s[i+1] != 'a':
-                    s[i] = 'a'
-                else:
-                    s[i] = 'b'
-                continue
-            if i == n-1:
-                if s[i-1] == 'a':
-                    s[i] = 'b'
-                else:
-                    s[i] = 'a'
-                continue
             if s[i-1] != 'a' and s[i+1] != 'a':
                 s[i] = 'a'
             else:
                 s[i] = 'b'
+    if s[-1] == '?':
+        if s[-2] == 'a':
+            s[-1] = 'b'
+        else:
+            s[-1] = 'a'
 else:
     s[0] = 'a'
 print(''.join(s))
