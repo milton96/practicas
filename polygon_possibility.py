e2c51084c5efb99c0416acfd6e76f2f00a4a927e
Milton Bravo Sanchez <mbravo@isavanzados.com.mx>
2019-09-23 17:41:00 -0500

    ejercicio aceptado, mejorado

--- polygon_possibility.py
+++ polygon_possibility.py
@@ -2,13 +2,9 @@ t = int(input())
 for i in range(t):
     n = int(input())
     l = list(map(int, input().split()))
-    x = sum(l)
-    f = 0
-    for j in l:
-        if j >= x-j:
-            f = 1
-            break
-    if f is 0:
+    m = max(l)
+    x = sum(l) - m
+    if m < x:
         print("Yes")
     else:
         print("No")
